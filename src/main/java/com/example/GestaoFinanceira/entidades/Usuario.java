766092238319ac018e1b63edc8f27775c5fd5d73
AugustoB1 <guto.brito280802@gmail.com>
2025-05-13 20:22:52 -0300

    Criando usuario

---
+++ src/main/java/com/example/GestaoFinanceira/entidades/Usuario.java
@@ -0,0 +1,29 @@
+package com.example.GestaoFinanceira.entidades;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name = "usuarios")
+@Entity(name = "usuarios")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String nome;
+    private String email;
+    private String cpf;
+    private String senha;
+
+    public Usuario(String nome, String email, String cpf, String senha) {
+        this.nome = nome;
+        this.email = email;
+        this.cpf = cpf;
+        this.senha = senha;
+    }
+}
