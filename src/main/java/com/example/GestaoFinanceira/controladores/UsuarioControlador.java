871424005f04c7e518bb814b3811079403b01460
AugustoB1 <guto.brito280802@gmail.com>
2025-05-15 20:24:21 -0300

    criando controlador de login

--- src/main/java/com/example/GestaoFinanceira/controladores/UsuarioControlador.java
+++ src/main/java/com/example/GestaoFinanceira/controladores/UsuarioControlador.java
@@ -3,6 +3,8 @@ package com.example.GestaoFinanceira.controladores;
 import com.example.GestaoFinanceira.entidades.Usuario;
 import com.example.GestaoFinanceira.repositorios.UsuarioRepositorio;
 import com.example.GestaoFinanceira.servicos.UsuarioServico;
+import com.example.GestaoFinanceira.uteis.LoginRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -35,5 +37,17 @@ public class UsuarioControlador {
                 .fromCurrentRequest().path("/{id}")
                 .buildAndExpand(usuario1.getId()).toUri();
         return ResponseEntity.created(uri).body(usuario1);
+    }
+
+    @PostMapping("/logar")
+    public ResponseEntity<Usuario> logar(@RequestBody LoginRequest loginRequest,
+                                         HttpSession session){
+        Usuario usuario = servico.login(loginRequest.getEmail(), loginRequest.getSenha());
+        if (usuario != null){
+            session.setAttribute("usuarioLogado", usuario);
+            System.out.println("logado com sucesso");
+            return ResponseEntity.ok().body(usuario);
+        }
+        throw new RuntimeException();
     }
 }
